6a88704636d545a3c2b230db290b9ec468b54531
AuroraHJ <auroraherrerojuarez74@gmail.com>
2023-11-05 19:27:08 +0100

    Segona versió

--- flutter_forms_samples-master/lib/sources/segon_formulari.dart
+++ flutter_forms_samples-master/lib/sources/segon_formulari.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_form_builder/flutter_form_builder.dart';
-// import 'package:form_builder_validators/form_builder_validators.dart';
-// import 'package:intl/intl.dart';
 
 class SegonFormulari extends StatefulWidget {
   const SegonFormulari({Key? key}) : super(key: key);
@@ -29,80 +26,54 @@ class _SegonFormulariState extends State<SegonFormulari> {
           centerTitle: true,
         ),
         body: Theme(
-                data: Theme.of(context).copyWith(
-                  colorScheme: const ColorScheme.light(primary: Colors.blue),
-                ),
-                child: Stepper(
-                  type: StepperType.horizontal,
-                  steps: getSteps(),
-                  currentStep: currentStep,
-                  onStepContinue: () {
-                    final isLastStep = currentStep == getSteps().length - 1;
-                    if (isLastStep) {
-                      setState(() => isCompleted = true);
-                      print('Completed');
-                    } else {
-                      setState(() => currentStep += 1);
-                    }
-                  },
-                  onStepTapped: (step) => setState(() => currentStep = step),
-                  onStepCancel: currentStep == 0
-                      ? null
-                      : () => setState(() => currentStep -= 1),
-                  controlsBuilder:
-                      (BuildContext context, ControlsDetails controlsDetails) {
-                    final isLastStep = currentStep == getSteps().length - 1;
-                    return Container(
-                      margin: const EdgeInsets.only(top: 50),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: ElevatedButton(
-                              onPressed: controlsDetails.onStepContinue,
-                              child: Text(isLastStep ? 'CONFIRM' : 'NEXT'),
-                            ),
-                          ),
-                          const SizedBox(width: 12),
-                          if (currentStep != 0)
-                            Expanded(
-                              child: ElevatedButton(
-                                onPressed: controlsDetails.onStepCancel,
-                                child: const Text('BACK'),
-                              ),
-                            ),
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              ),
-      );
-/*        body: Container(
-          padding: const EdgeInsets.all(20),
-          child: ListView(
-            children: [
-              const CustomInput(
-                hint: "Email",
-                inputBorder: OutlineInputBorder(),
-              ),
-              const CustomInput(
-                hint: "Address",
-                inputBorder: OutlineInputBorder(),
-              ),
-              const CustomInput(
-                hint: "Mobile No",
-                inputBorder: OutlineInputBorder(),
-              ),
-              CustomBtn(
-                title: const Text(
-                  "Save",
-                  style: TextStyle(color: Colors.white),
-                ),
-                callback: () {},
-              )
-            ],
+          data: Theme.of(context).copyWith(
+            colorScheme: const ColorScheme.light(primary: Colors.blue),
           ),
-        ),*/
+          child: Stepper(
+            type: StepperType.horizontal,
+            steps: getSteps(),
+            currentStep: currentStep,
+            onStepContinue: () {
+              final isLastStep = currentStep == getSteps().length - 1;
+              if (isLastStep) {
+                setState(() => isCompleted = true);
+                print('Completed');
+              } else {
+                setState(() => currentStep += 1);
+              }
+            },
+            onStepTapped: (step) => setState(() => currentStep = step),
+            onStepCancel: currentStep == 0
+                ? null
+                : () => setState(() => currentStep -= 1),
+            controlsBuilder:
+                (BuildContext context, ControlsDetails controlsDetails) {
+              final isLastStep = currentStep == getSteps().length - 1;
+              return Container(
+                margin: const EdgeInsets.only(top: 50),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: controlsDetails.onStepContinue,
+                        child: Text(isLastStep ? 'CONFIRM' : 'NEXT'),
+                      ),
+                    ),
+                    const SizedBox(width: 12),
+                    if (currentStep != 0)
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: controlsDetails.onStepCancel,
+                          child: const Text('BACK'),
+                        ),
+                      ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      );
   List<Step> getSteps() => [
         Step(
           state: currentStep > 0 ? StepState.complete : StepState.indexed,
@@ -156,44 +127,3 @@ class _SegonFormulariState extends State<SegonFormulari> {
         ),
       ];
 }
-/*  class CustomInput extends StatelessWidget {
-    final ValueChanged<String>? onChanged;
-    final String? hint;
-    final InputBorder? inputBorder;
-    const CustomInput({Key? key, this.onChanged, this.hint, this.inputBorder})
-        : super(key: key);
-
-    @override
-    Widget build(BuildContext context) {
-      return Container(
-        margin: const EdgeInsets.only(bottom: 10),
-        child: TextField(
-          onChanged: (v) => onChanged!(v),
-          decoration: InputDecoration(hintText: hint!, border: inputBorder),
-        ),
-      );
-    }
-  }
-
-  class CustomBtn extends StatelessWidget {
-    final Function? callback;
-    final Widget? title;
-    const CustomBtn({Key? key, this.title, this.callback}) : super(key: key);
-
-    @override
-    Widget build(BuildContext context) {
-      return Container(
-        margin: const EdgeInsets.only(bottom: 10),
-        child: SizedBox(
-          width: double.infinity,
-          child: Container(
-            color: Colors.blue,
-            child: TextButton(
-              onPressed: () => callback!(),
-              child: title!,
-            ),
-          ),
-        ),
-      );
-    }
-  }*/
